7f95c9ac06ef86d18859a3afbbc459aae4b09e37
glorieux <glorieux@be95da7d-930d-430d-86f6-0ab18130325d>
2014-05-24 07:02:40 +0000

    Mettre au point un build utilisable : * en ssh * par php * par svn

--- build.sh
+++ build.sh
@@ -1,1 +1,7 @@
-php teipub/Teipub.php "xml/*.xml" crht.sqlite
+#!/bin/sh
+BASEDIR=$(dirname $0)
+# pour les noms de fichiers avec accents
+export LC_CTYPE=fr_FR.UTF-8
+/usr/bin/svn --non-interactive --trust-server-cert  update $BASEDIR/ 2>&1
+/usr/bin/php $BASEDIR/teipub/Teipub.php  "$BASEDIR/xml/*.xml"  "$BASEDIR/crht.sqlite" 2>&1
+exit 0
