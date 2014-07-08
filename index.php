8768013ea783502d181a5cf290b3710b0c06fd24
glorieux <glorieux@be95da7d-930d-430d-86f6-0ab18130325d>
2014-07-08 14:43:07 +0000

    Editor

--- index.php
+++ index.php
@@ -53,7 +53,7 @@ if (isset($doc['body'])) {
 else {
   $pot->search(); // charger des résultats en mémoire
   echo $pot->report(); // nombre de résultats
-  echo $pot->biblio(array('byline', 'title', 'date', 'download')); // présentation bibliographique des résultats
+  echo $pot->biblio(array('byline', 'title', 'date', 'editor', 'download')); // présentation bibliographique des résultats
   echo $pot->concByBook(); // concordance s’il y a recherche plein texte
   
   ?><div class="linkOld" style="width: 100%; text-align: center;"><a style="color: gray; font-size: 14px; border-bottom: none;" href="http://www.crht.paris-sorbonne.fr/">Suite des pièces…</a></div><?php
